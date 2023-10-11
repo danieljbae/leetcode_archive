f71e1ce5c2ea867c5e4fe6c8f357c42b3fd09172
Daniel Bae <danieljbaee@hotmail.com>
2023-10-10 17:59:53 -0700

    Added: q16

---
+++ concepts/pointerBased/twoPointer/q16_3SumClosest.py
@@ -0,0 +1,54 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        """
+        nums = [-1,2,1,-4], target = 1
+        output = 2
+
+        explanation: the sum that is closest to the target is 2 = -1 + 2 + 1 
+        __________________________________________________________________
+
+        target = 1
+
+         i    l     r
+        [-4, -1, 1, 2]
+
+        for loop to hold i constant
+            curr_sum = sum(nums[i], nums[l], nums[r]) = -3
+
+
+            while loop and perform 2 pointer to search for closest difference
+
+                curr_diff = abs(target-curr_sum)
+
+                if curr_diff < closest_diff:
+                    closest_sum = curr_sum
+
+                search for next closest diff with i being held as constant
+
+        """
+        nums.sort()
+        closest_sum = float('inf')
+        n = len(nums)
+
+        for i, num in enumerate(nums[:-2]):
+            left, right = i+1, n-1
+            # Hold i constant and Use 2-pointer to find closest difference
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+
+                # Update if closer difference found
+                if abs(target-curr_sum) < abs(target-closest_sum):
+                    print(num, nums[left], nums[right])
+                    closest_sum = curr_sum
+
+                # Search for next closer difference
+                if curr_sum < target:
+                    left += 1
+                elif curr_sum > target:
+                    right -= 1
+
+                # Found triplet that equals target
+                else:
+                    return 0
+
+        return -1 if closest_sum == float('inf') else int(closest_sum)
